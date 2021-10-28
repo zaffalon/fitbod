89b214fa906d116a4c80d6390ddc2e7e3046c54c
Murilo Zaffalon Marra <mzaffalon@hotmail.com>
2021-10-27 23:25:03 -0300

    create users and workouts migration

---
+++ db/migrate/20211027225343_create_workouts.rb
@@ -0,0 +1,11 @@
+class CreateWorkouts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :workouts, id: :uuid  do |t|
+      t.references :user
+      t.datetime :workout_at
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
